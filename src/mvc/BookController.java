166012e9e8aaa115d04c072b9366442af6db606e
lior <lior_s99@outlook.com>
2019-04-08 21:14:28 -0600

    Revert "Add files via upload"
    This reverts commit df5a7ba19f594ad0d973b7563b135314c2db2c76.

--- src/mvc/BookController.java
+++ src/mvc/BookController.java
@@ -1,11 +1,17 @@
-package user.student;
+package mvc;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import database.BooksAndStuff;
 import database.User;
 
-public class BookController {
+public class BookController implements ControllerInterface{
 	//	@Override
 	public BooksAndStuff book;
 	public Book_view view;
@@ -21,26 +27,70 @@ public class BookController {
 		view.name_label.setText(book.getName());
 		view.author_label.setText(book.getAuthor());
 		
+		switch(view.getButtonType()) {
+			case "Borrow":
+				view.addOrderListener(new orderListener());
+				break;
+			case "Return":
+				break;
+			default:
+				break;
 		
-		view.addOrderListener(new orderListener());
+		}
+			
+		
+		
 		view.addBackListener(new backToSearchListener());
 	}
 	
 	public class orderListener implements ActionListener{
-	public void actionPerformed(ActionEvent arg0) {
-		
-		int total = book.getTotal();
-		int numCheckedOut = book.getCheckedOut();
-		if(numCheckedOut >= total) {
-			System.out.println("All copies are currently unavailable");
-		}else {
-			System.out.println("Book has been reserved!");
+		public void actionPerformed(ActionEvent arg0) {
 			
-			//User.getData().performQuery("SELECT id");
-			//book.getDataParser().performInsert("INSERT INTO rentals (idrental, rental_Date, iditem, idrenter, lastupdate) VALUES (" + User.name )
-			book.getDataParser().performUpdate("UPDATE books_and_others SET qCheckedOut = qCheckedOut+1 WHERE name = '" + book.getName() + "'" );
-			System.out.println("test");
-		}
+			int total = book.getTotal();
+			int numCheckedOut = book.getCheckedOut();
+			
+			int rentCount = 0;
+			
+			try {
+			 ResultSet rents = book.getDataParser().performQuery("SELECT userRenter FROM rentalObj WHERE userRenter = '" + current_user.getCurrentUserName() + "'");//.getObject();
+			
+			 while(rents.next()) {
+				 rentCount++;
+			 }
+			
+			}catch(SQLException ex) {
+				
+			}
+			
+			if(numCheckedOut >= total) {
+				System.out.println("All copies are currently unavailable");
+			}else if(rentCount >= 5){
+				System.out.println("You have exceeded your rental capacity");
+			}
+			
+			else{
+				
+				boolean isBook = false;
+				boolean isItem = false;
+				boolean isRenter = false;
+				boolean isLibrary = false;
+				boolean isPayment = false;
+				
+				
+				book.getDataParser().createTable();
+
+				DateFormat dateForm = new  SimpleDateFormat("yyyy-MM-dd");
+				Date date = new Date();
+				String dateStr = dateForm.format(date);
+				System.out.println(dateStr);
+				
+				
+				book.getDataParser().performInsert("INSERT INTO rentalObj (userRenter, itemRented, dateRented) VALUES ('" + current_user.getCurrentUserName() + "', " + book.getId() + ", DATE '" + dateStr + "')");
+				System.out.println("Book has been reserved!");
+				book.getDataParser().performUpdate("UPDATE books_and_others SET qCheckedOut = " + (numCheckedOut + 1) + " WHERE name = '" + book.getName() + "'" );
+
+			}
+			
 	}
 	
 	
@@ -48,14 +98,11 @@ public class BookController {
 
 }
 	
-public class backToSearchListener implements ActionListener {
-
-public void actionPerformed(ActionEvent arg0) {
-	view.frame.dispose();
-	//frame.dispose();
-//	Borrow_frame borrow = new Borrow_frame();
-//	borrow.frame.setVisible(true);
-}
-
-}
+	public class backToSearchListener implements ActionListener {
+	
+		public void actionPerformed(ActionEvent arg0) {
+			view.frame.dispose();
+		}
+	
+	}
 }
